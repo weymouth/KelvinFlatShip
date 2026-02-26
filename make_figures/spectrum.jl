3b9800e83a42f543770c8b9373c2d31e0ae60988
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2026-02-26 17:07:01 +0100

    Fix signs by getting the Hankel branch cuts correct

--- make_figures/spectrum.jl
+++ make_figures/spectrum.jl
@@ -9,9 +9,9 @@ using FFTW
 function spectrum(y,f)
     dy,N = step(y),length(y)
     w = @. 0.5*(1 + cos(pi*(0:N-1)/(N-1)))
-    S = abs2.(fft(f.*w)) * dy^2
+    S² = abs2.(fft(f.*w)) * dy^2
     k = fftfreq(N, 1/dy) .* 2π
-    k[2:end÷2],S[2:end÷2]
+    k[2:end÷2],S²[2:end÷2]
 end
 
 # make plot
@@ -22,22 +22,22 @@ begin
     plot!(plt[1],xlabel="y",ylabel="∂ₓW")
     plot!(plt[2],xlabel="k",ylabel="|S|²", yscale=:log10, xscale=:log10)
 
-    # # Point-source over depths
-    # for n in 3:6
-    #     z = -0.46416^n
-    #     # plt[1] spacial domain, not too far away and the whole width
-    #     x=-8; y = range(-1-x/√8,1+x/√8,2^10)
-    #     plot!(plt[1],y,∂ₓW.(x,y,z),label="z=$(round(z, digits=3))",c=colormap("Blues",6)[n])
+    # Point-source over depths
+    for n in 3:6
+        z = -0.46416^n
+        # plt[1] spacial domain, not too far away and the whole width
+        x=-8; y = range(-2+x/√8,2-x/√8,2^10)
+        plot!(plt[1],y,∂ₓW.(x,y,z),label="z=$(round(z, digits=3))",c=colormap("Blues",6)[n])
 
-    #     # plt[2] spectral domain, much farther and only through 1/4 of the width
-    #     x=-80; y = range(0,-x/4√2,2^14)
-    #     plot!(plt[2],spectrum(y,∂ₓW.(x,y,z))...,label="",c=colormap("Blues",6)[n])
-    # end
+        # plt[2] spectral domain, much farther and only through 1/4 of the width
+        x=-40; y = range(0,-x/4√2,2^14)
+        plot!(plt[2],spectrum(y,∂ₓW.(x,y,z))...,label="",c=colormap("Blues",6)[n])
+    end
 
     # Elliptic line-source on z=-0.
-    z=-0.; x=-8.; y = range(-1-x/√8,1+x/√8,2^10)
-    plot!(plt[1],y,∂ₓWᵦ.(x,y,z),label="flatship z=0",c=:forest)
-    x=-80; y = range(0,-x/4√2,2^14)
-    plot!(plt[2],spectrum(y,∂ₓW.(x,y,z))...,label="",c=:forest)
+    z=-0.; x=-8.; y = range(-2+x/√8,2-x/√8,2^10)
+    plot!(plt[1],y,∂ₓWᵦ.(x,y,z),label="line-integrated z=0",c=:forestgreen)
+    x=-40; y = range(0,-x/4√2,2^14)
+    plot!(plt[2],spectrum(y,∂ₓWᵦ.(x,y,z))...,label="",c=:forestgreen)
 end;plt
 savefig(plt,"point_spectrum.png")
--- src/flatship.jl
+++ src/flatship.jl
@@ -5,7 +5,7 @@ using TupleTools
 using NeumannKelvin: kₓ,nearfield
 k(t) = t*kₓ(t)
 γj(t,b) = abs(t)<√eps(typeof(abs(t))) ? b/2 : SpecialFunctions.besselj1(b*k(t))/k(t)
-γhx(t,b) = SpecialFunctions.besselhx(1,1,b*k(t))/2k(t) # can't use near t=0
+γhx(t,p,b) = SpecialFunctions.besselhx(1,p,b*k(t))/2k(t) # can't use near t=0
 """
 ∫₂wavelike(x,y,z) = ∫ √(1-(y′/b)^2) W(x,y-y′,z) dy′
 
@@ -19,37 +19,41 @@ where `t∈[-∞,∞], `g(x,y,t)=(x+yt)√(1+t²)`, and `H` is the Heaviside fun
 
 where `J₁` is the Bessel function of the first kind and `k(t)=t√(1+t^2)`. This is better regularized than the original integral for z≈0⁻, but still requires the use of a complex path of integration to avoid wasting 1000s of function evaluations on the decaying tail oscillations. Away from the stationary points and t=0 we use the identity
 
-    J₁(z) = (H₁¹(z) exp(iz) + H₁¹(-z) exp(-iz))/2
+    J₁(z) = (H₁¹(z) exp(iz) + H₁²(z) exp(-iz))/2
 
-where `H₁¹` is the scaled Hankel function of the first kind. This function is treated as slowly varying prefactors while the exponentials are absorbed into the complex steepest descent path.
+where `H₁¹` and `H₁²` are the scaled Hankel functions of the first and second kind respectively. These functions are are treated as slowly varying prefactors while the exponentials are absorbed into the complex steepest descent path.
 """
 function ∫₂wavelike(x,y,z;b=1,ltol=-10,Δg=6)
     x,y,z = promote(x,y,z)
-    (x≥0 || z≤ltol) && return zero(x)                            # no waves
+    (x≥0 || z≤ltol) && return zero(x)                             # no waves
     abs(y) > b-x/√8 && return π*wavelike(x,abs(y),z,γ=t->γj(t,b)) # fast treatment outside the wake
 
     # Find the stationary points and the finite ranges around them
-    xv,yv,zv = value.((x,y,z))            # strip Duals
-    R,atol = √(ltol/zv-1),10exp(ltol)     # angle limit and absolute tolerance
-    rngs = mapreduce(vcat,(-b,b)) do y′   # merge three phase ranges
+    xv,yv,zv = value.((x,y,z))               # strip Duals
+    atol = exp(ltol)                         # absolute tolerance
+    R = √min(ltol/zv-1,(2/π/b/atol^2)^(1/3)) # angle limit
+    rngs = mapreduce(vcat,(-b,b)) do y′      # merge phase ranges
         S = TupleTools.sort(filter(s->-R<s<R,(S₀(xv,yv+y′)...,zero(xv))))
-        rng = finite_ranges(S,t->g(xv,yv+y′,t),Δg,R;atol)
-        rng2int(first.(rng))
+        rng = finite_ranges(S,t->g(xv,yv+y′,t),Δg,R;atol) .|> first |> interval
     end |> sort |> merge
 
     # Real-line integrand and complex path phases and pre-factors
     f(t) = γj(t,b)*exp(z*(1+t^2))*sin(g(x,y,t))
-    g₊(t)=g(x,y+b,t)-im*z*(1+t^2); dg₊(t)=dg(x,y+b,t)-2im*z*t; γ₊(t)=γhx(t,b)
-    g₋(t)=g(x,y-b,t)-im*z*(1+t^2); dg₋(t)=dg(x,y-b,t)-2im*z*t; γ₋(t)=γhx(t,-b)
+    g₊(t)=g(x,y+b,t)-im*z*(1+t^2); dg₊(t)=dg(x,y+b,t)-2im*z*t; γ₊(t)=real(k(t)) > 0 ? γhx(t,1,b) : -γhx(t,2,-b)
+    g₋(t)=g(x,y-b,t)-im*z*(1+t^2); dg₋(t)=dg(x,y-b,t)-2im*z*t; γ₋(t)=real(k(t)) > 0 ? γhx(t,2,b) : -γhx(t,1,-b)
+    tail(t₀) = nsd(t₀,g₊,dg₊,γ₊)+nsd(t₀,g₋,dg₋,γ₋)
 
     # Sum over finite ranges and semi-infinite tails
     4π*sum(rngs) do (t₁,t₂)
-        quadgk(f,t₁,t₂)[1] + sign(y+b)*(nsd(t₁,g₊,dg₊,γ₊)+nsd(t₁,g₋,dg₋,γ₋)) + sign(b-y)*(nsd(t₂,g₊,dg₊,γ₊)+nsd(t₂,g₋,dg₋,γ₋))
+        quadgk(f,t₁,t₂)[1] -tail(t₁) + tail(t₂)
     end
 end
-rng2int(rngs::NTuple{N}) where N = [(rngs[2i-1],rngs[2i][1]) for i in 1:N÷2]
+
+interval(rngs::NTuple{N}) where N = [(rngs[2i-1],rngs[2i][1]) for i in 1:N÷2]
 merge(intervals) = foldl(intervals[2:end], init=intervals[1:1]) do acc, (a,b)
-    a > acc[end][2] ? push(acc, (a,b)) : (acc[end] = (acc[end][1], max(acc[end][2], b)); acc)
+    c,d = acc[end]
+    a > d ? push!(acc, (a,b)) : (acc[end] = (c, max(d, b)))
+    acc
 end
 
 # Brute-force version for comparison
@@ -63,13 +67,13 @@ begin
 end
 
 # Check the two ∫₂wavelike implementations give the same answer and compare timings
-function check(y,x=-1.,z=-0.)
+function flatship_check(y,x=-1.,z=-0.)
     wavelike = @btimed ∫₂wavelike($x,$y,$z) seconds=0.1
     brute = @timed brute∫₂wavelike(x,y,z)
     println("y = $y: wavelike = $(wavelike.value), brute = $(brute.value), wavelike time = $(wavelike.time) seconds, brute time = $(brute.time) seconds")
     (y=y, abserror = abs(wavelike.value-brute.value), relerror = abs(wavelike.value/brute.value-1), speedup = brute.time/wavelike.time)
 end
-flatship_table()=Table(check(y) for y in (0.,0.5,0.9,1.1,2.,4.))
+flatship_table()=Table(flatship_check(y) for y in (0.,0.5,0.9,1.1,2.,4.))
 
 # Add the near-field contribution using direct integration
 ∫₂kelvin(x,y,z,b=1) = ∫₂wavelike(x,y,z;b)+quadgk(y′->√(1-(y′/b)^2)*nearfield(x,y-y′,z),-b,b;atol=1e-4)[1]
