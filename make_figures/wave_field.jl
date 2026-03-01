4f00f6e2628ddb9e0acfc42b7365eec84da02cc8
weymouth <gabriel.weymouth@gmail.com>
2026-03-01 16:36:21 +0100

    update wave fields

--- make_figures/wave_field.jl
+++ make_figures/wave_field.jl
@@ -4,9 +4,9 @@ include("../src/flatship.jl")
 
 # make plot
 using Plots
-for b in (1,2.5)
-    plot(xlabel="x",ylabel="y",size=(600,600));
-    contour!(-20:0.1:1,-10:0.05:10,(x,y)->ζ(x,y,b)-ζ(x+5,y,b),levels=-8.5:1:8.5,clims=(-9,9))
-    plot!(Shape([(0,b),(0,-b),(-5,-b),(-5,b)]),label="",c=:black,alpha=0.5,aspect_ratio=1)
+h,ζm = 0.05,8.5
+for b in (1,2)
+    contourf(-20:h:1,-10:h:10,(x,y)->clamp(ζ(x,y,b)-ζ(x+5,y,b),-ζm,ζm),levels=18,clims=(-ζm,ζm),colormap=:balance,line=0)
+    plot!(Shape([(0,b),(0,-b),(-5,-b),(-5,b)]),label="",c=:black,alpha=0.5,aspect_ratio=1,xlabel="x",ylabel="y",size=(600,600));
     savefig("wave_field_b$(b).png")
 end
