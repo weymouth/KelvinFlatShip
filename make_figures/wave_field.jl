7430a866f3050ce9f9531377d1bbdd2a93c29aa4
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2026-03-02 12:45:49 +0100

    Multi-threaded wave plots

--- make_figures/wave_field.jl
+++ make_figures/wave_field.jl
@@ -1,12 +1,19 @@
 # wave stuff
 include("../src/flatship.jl")
-ζ(x,y,b) = derivative(x->∫₂kelvin(x,y,-0.,b)*2/(π*b),x)
+import AcceleratedKernels as AK
+function ζ(x,y,b)  # multi-threaded evaluation of ζ
+    z = Array{Float64}(undef,length(x),length(y))
+    AK.foraxes(z,1) do i; for j in axes(z,2) 
+        z[i,j] = derivative(x->∫₂kelvin(x,y[j],-0.,b),x[i])
+    end; end; z
+end
 
 # make plot
 using Plots
-h,ζm = 0.05,8.5
-for b in (1,2)
-    contourf(-20:h:1,-10:h:10,(x,y)->clamp(ζ(x,y,b)-ζ(x+5,y,b),-ζm,ζm),levels=18,clims=(-ζm,ζm),colormap=:balance,line=0)
-    plot!(Shape([(0,b),(0,-b),(-5,-b),(-5,b)]),label="",c=:black,alpha=0.5,aspect_ratio=1,xlabel="x",ylabel="y",size=(600,600));
+ζm = 8.5
+for (b,h) in ((1,0.05),(2,0.1))
+    x=-20:h:1; y=-10:h:10; z = clamp.((ζ(x,y,b)-ζ(x .+ 5,y,b)) .* 2/(π*b),-ζm,ζm)
+    contourf(x,y,z',levels=18,clims=(-ζm,ζm),colormap=:balance,line=0)
+    plot!(Shape([(0,b),(0,-b),(-5,-b),(-5,b)]),label="",c=:black,alpha=0.5,aspect_ratio=1,xlabel="x",ylabel="y",size=(600,600))
     savefig("wave_field_b$(b).png")
 end
