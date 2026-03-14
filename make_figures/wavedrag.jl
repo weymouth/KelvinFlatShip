e4fd4da1626208862f074b4108c09d5d9dcf9537
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2026-03-14 08:46:13 +0100

    fix wave drag labels

--- make_figures/wavedrag.jl
+++ make_figures/wavedrag.jl
@@ -6,17 +6,18 @@ Cw(L,b) = 4π*quadgk(t->SpecialFunctions.besselj1(b*k(t))^2/(b*k(t))^2*kₓ(t)*(
 import AcceleratedKernels as AK
 function Cw(L::AbstractArray,b::AbstractArray)
     z = Array{Float64}(undef,length(L),length(b))
-    AK.foraxes(z,1) do i; for j in axes(z,2) 
+    AK.foraxes(z,1) do i; for j in axes(z,2)
         z[i,j] = Cw(L[i],b[j])
     end; end; z
 end
 
 # make plot
-using Plots
+using Plots,LaTeXStrings
 L=range(π/10,6π,200); b=logrange(0.1,10,100)
 z = Cw(L,b)
 contourf(L,b,z',levels=18,lw=0,cmap=:amp,clims=(0,30),
-    ylabel="      b",yscale=:log10,yguidefontrotation=-90,
-    xlabel="L",xticks=([π,3π,5π],["π","3π","5π"]),
-    size=(800,300),colorbar_title="\nCw/q₀²",rightmargin=5Plots.mm)
+    ylabel="       "*L"b",yscale=:log10,yguidefontrotation=-90,
+    xlabel=L"L",xticks=([π,3π,5π],["π","3π","5π"]),
+    size=(800,300),colorbar_title=L"C_W/q_0^2",
+    rightmargin=3Plots.mm,bottommargin=3Plots.mm,labelfontsize=12)
 savefig("wave_drag.png")
